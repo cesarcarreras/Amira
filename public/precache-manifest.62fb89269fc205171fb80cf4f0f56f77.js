b9afa0b9c4eb7fb7b4275b0e4b06ec73bc818e02
Cesar Carreras <cesar.carreras95@gmail.com>
2021-09-17 03:53:35 -0500

    new build

---
+++ public/precache-manifest.62fb89269fc205171fb80cf4f0f56f77.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a289d1648d9a5609c7075108276331",
+    "url": "/index.html"
+  },
+  {
+    "revision": "991f4b10b0a60debd6ab",
+    "url": "/static/css/main.80464ce4.chunk.css"
+  },
+  {
+    "revision": "06f8c2ff9258e6079a96",
+    "url": "/static/js/2.3b514f7f.chunk.js"
+  },
+  {
+    "revision": "5b134647b016f7a660e7",
+    "url": "/static/js/3.ddcdb50c.chunk.js"
+  },
+  {
+    "revision": "674d856204cd459cc5de",
+    "url": "/static/js/4.4a57c7e4.chunk.js"
+  },
+  {
+    "revision": "991f4b10b0a60debd6ab",
+    "url": "/static/js/main.ce236854.chunk.js"
+  },
+  {
+    "revision": "e245e26739c6179b30c6",
+    "url": "/static/js/runtime-main.49ae8587.js"
+  },
+  {
+    "revision": "ebd9ca96e4fdfeb166d0852e495da1cc",
+    "url": "/static/media/background-products.ebd9ca96.jpg"
+  },
+  {
+    "revision": "486f65a451105d9b926f86b7202dbde5",
+    "url": "/static/media/banner2.486f65a4.png"
+  },
+  {
+    "revision": "ff747c3f55bbb1a028f981ecd98240bc",
+    "url": "/static/media/lavanda-palo-rosa.ff747c3f.png"
+  },
+  {
+    "revision": "ebb4155cad212707c978e1d53462ec17",
+    "url": "/static/media/logo.ebb4155c.png"
+  }
+]);
